365f10d630abb75693e1ae60f5699eb98ca1f7d4
Yongmei Chen <yongmei.chen@mycit.ie>
2020-11-18 01:09:15 +0000

    login.html authentication with firebase

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from flask import *
 import pyrebase
 
 config = {
@@ -13,30 +14,44 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
-
 db = firebase.database()
 
-db.child("names").push({"name":"mei"})
+# db.child("names").push({"name" : "mei"})
+# db.child("names").child("name").update({"name":"chen"})
+# users = db.child("names").child("name").get()
+# print(users.val())
+# db.child("names").remove()  #deletes that column
 
 app = Flask(__name__)
+auth = firebase.auth()
 
 
-@app.route('/')
+@app.route('/webintro.html', methods=['GET', 'POST'])
 def hello_world():
-
     return render_template('webintro.html')
 
-@app.route('/login.html')
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
+    unsuccessful = 'Please check your credentials'
+    successful = 'Login successful'
+    if request.method == 'POST':
+        email = request.form['name']
+        password = request.form['pass']
+        try:
+            auth.sign_in_with_email_and_password(email, password)
+            return render_template('new.html', s=successful)
+        except:
+            return render_template('new.html', us=unsuccessful)
     return render_template('login.html')
 
 
-@app.route('/register.html')
+@app.route('/register.html', methods=['GET', 'POST'])
 def register():
     return render_template('register.html')
 
 
-@app.route('/mainpage.html')
+@app.route('/mainpage.html', methods=['GET', 'POST'])
 def mainpage():
     return render_template('mainpage.html')
 
